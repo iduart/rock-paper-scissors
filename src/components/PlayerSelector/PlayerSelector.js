26b1d2955e3781a269b78010c789fb99832b28f7
Iduart Dean <iduartdean@gmail.com>
2019-03-30 16:08:50 -0500

    Add player selector page and customized some styles

--- src/components/PlayerSelector/PlayerSelector.js
+++ src/components/PlayerSelector/PlayerSelector.js
@@ -1,10 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PlayerSelectorSvg from './PlayerSelectorSvg';
+import './PlayerSelector.scss';
 
 const PlayerSelector = () => {
+  const [player1, setPlayer1] = useState('');
+  const [player2, setPlayer2] = useState('');
+
+  const onStartMatch = () => {
+    console.log('started');
+  };
+
   return (
-    <PlayerSelectorSvg />
-  )
-}
+    <PlayerSelectorSvg onStartMatch={onStartMatch}>
+      <input
+        type='text'
+        tabIndex='1'
+        className='playerInput'
+        onChange={e => setPlayer1(e.target.value)}
+      />
+      <input
+        type='text'
+        tabIndex='2'
+        className='playerInput'
+        onChange={e => setPlayer2(e.target.value)}
+      />
+    </PlayerSelectorSvg>
+  );
+};
 
 export default PlayerSelector;
