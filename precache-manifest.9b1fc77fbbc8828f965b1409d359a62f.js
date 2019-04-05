4e1852373dadd253a6c17f0ea455573577dbfb83
ideanb <iduartdean@gmail.com>
2019-04-05 10:16:51 -0500

    Updates

---
+++ precache-manifest.9b1fc77fbbc8828f965b1409d359a62f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "38e978e3b92294d459a1",
+    "url": "/rock-paper-scissors/static/css/main.d97e7a65.chunk.css"
+  },
+  {
+    "revision": "38e978e3b92294d459a1",
+    "url": "/rock-paper-scissors/static/js/main.1d52a79e.chunk.js"
+  },
+  {
+    "revision": "cf8ffcfd822dc753bb49",
+    "url": "/rock-paper-scissors/static/js/runtime~main.c4e15f11.js"
+  },
+  {
+    "revision": "bb06b06a225700fadd21",
+    "url": "/rock-paper-scissors/static/js/2.998302f9.chunk.js"
+  },
+  {
+    "revision": "388e33e3d2facf2dfbeb240b29c427e6",
+    "url": "/rock-paper-scissors/index.html"
+  }
+];
